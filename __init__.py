8545cafa5f748c04936dcbf435a5d78474ad8d2b
resteve <resteve@zikzakmedia.com>
2014-02-06 12:10:22 +0100

    Shipment comment in company

--- __init__.py
+++ __init__.py
@@ -2,12 +2,14 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
+from .company import *
 from .shipment import *
 from .party import *
 
 
 def register():
     Pool.register(
+        Company,
         ShipmentIn,
         ShipmentInReturn,
         ShipmentOut,
---
+++ company.py
@@ -0,0 +1,14 @@
+# This file is part of the stock_comment module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
+
+__all__ = ['Company']
+__metaclass__ = PoolMeta
+
+
+class Company:
+    __name__ = 'company.company'
+    comment_shipment = fields.Text('Shipment Comment')
