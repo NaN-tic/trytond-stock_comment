f7955f26bc2c70ea1adf35cafca0be27057f50dd
resteve <resteve@zikzakmedia.com>
2015-12-01 11:23:33 +0100

    Tests

--- tests/test_stock_comment.py
+++ tests/test_stock_comment.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part stock_comment module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_comment module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockCommentTestCase(unittest.TestCase):
+class StockCommentTestCase(ModuleTestCase):
     'Test Stock Comment module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_comment')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_comment')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_comment'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockCommentTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
