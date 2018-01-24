57fa19ac888a0e7f47ac2a0e872bdc30a7ed213d
C?dric Krier <ced@b2ck.com>
2018-01-24 15:51:40 +0100

    Remove volumes from drone and fix test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 # This file is part stock_comment module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from .test_stock_comment import suite
+try:
+    from trytond.modules.stock_comment.tests.test_stock_comment import suite
+except ImportError:
+    from .test_stock_comment import suite
 
 __all__ = ['suite']
