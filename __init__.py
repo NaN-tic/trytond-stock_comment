d6404f2cb9241858dcd3b01ab7237cb9a28ae25b
C?dric Krier <ced@b2ck.com>
2018-01-24 15:51:47 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -2,19 +2,19 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .company import *
-from .shipment import *
-from .party import *
+from . import company
+from . import shipment
+from . import party
 
 
 def register():
     Pool.register(
-        Company,
-        ShipmentIn,
-        ShipmentInReturn,
-        ShipmentOut,
-        ShipmentOutReturn,
-        ShipmentInternal,
-        Party,
-        Address,
+        company.Company,
+        shipment.ShipmentIn,
+        shipment.ShipmentInReturn,
+        shipment.ShipmentOut,
+        shipment.ShipmentOutReturn,
+        shipment.ShipmentInternal,
+        party.Party,
+        party.Address,
         module='stock_comment', type_='model')
